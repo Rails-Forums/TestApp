b4c1b35817c64031fe4fba5db5e4d9777cab0c8e
leo-midili <charmandermaster07@gmail.com>
2021-06-22 16:16:17 -0400

    Also dont forget to upload your work to github this is how you do it.

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,6 @@
+class HomesController < ApplicationController
+  def index
+    # Also you see how I repeated hey for the lists you can add this to it so you only see one thing
+    @h = "hey"
+  end
+end
